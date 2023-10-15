af8115b41301437f73ae853767e467457ee2afc2
Nekohime <Nekohime@users.noreply.github.com>
2023-10-15 11:06:44 -0400

    Lighter main.css

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 8888;
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assets'));
 
 
 const directories = [
